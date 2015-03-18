95357553744b20fa9a435f0d884a0d10b68361a6
x89 <napalm10@gmail.com>
2015-03-18 16:51:55 +0000

    Downvote + init. Don't forget to praw!

---
+++ downvote-psas.py
@@ -0,0 +1,11 @@
+# Every single "PSA:" is a joke and deserves to be buiried.
+# Works well with the Reddit option to "hide down-voted submissions"
+
+from praw import Reddit
+r = Reddit(user_agent="Die-PSA")
+r.login()
+
+for post in r.get_front_page():
+    if 'psa' in str.lower(post.title):
+        post.downvote()
+        print('Downvoted %s.' % post.title)
